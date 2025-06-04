12d63d3de59c29de331fc82ab2b3e014cc916d8a
chiancw <chiancw@gmail.com>
2025-06-04 14:36:44 +0800

    update dns subnet variable reference

--- dnsendpoints.subnets.tf
+++ dnsendpoints.subnets.tf
@@ -1,6 +1,6 @@
 resource "azurerm_subnet" "nprd_dns_inbound_subnet" {
   name                 = "nprd-dns-inbound-subnet"
-  resource_group_name  = var.custom_replacements.names.connectivity_hub_primary_resource_group_name
-  virtual_network_name = var.custom_replacements.names.primary_sidecar_virtual_network_name
+  resource_group_name  = var.custom_replacements.names.connectivity_hub_primary_resource_group_name.value
+  virtual_network_name = var.custom_replacements.names.primary_sidecar_virtual_network_name.value
   address_prefixes     = ["10.0.4.80/28"]
 }
