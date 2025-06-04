a37ef78350ca3e13008abf8a37cb3eb5da306425
chiancw <chiancw@gmail.com>
2025-06-04 21:48:58 +0800

    add dns subnet delegation

--- dnsendpoints.subnets.tf
+++ dnsendpoints.subnets.tf
@@ -4,6 +4,13 @@ resource "azurerm_subnet" "nprd_dns_inbound_subnet" {
   resource_group_name  = "rgbdo7-hub1-southeastasia"
   virtual_network_name = "bdo7vnet-sidecar1-southeastasia"
   address_prefixes     = ["10.0.4.80/28"]
+  delegation {
+    name = "dns-inbound-delegation"
+    service_delegation {
+      name    = "Microsoft.Network/dnsResolvers"
+      actions = ["Microsoft.Network/virtualNetworks/subnets/join/action"]
+    }
+  }
 }
 
 resource "azurerm_subnet" "nprd_dns_outbound_subnet" {
@@ -12,4 +19,11 @@ resource "azurerm_subnet" "nprd_dns_outbound_subnet" {
   resource_group_name  = "rgbdo7-hub1-southeastasia"
   virtual_network_name = "bdo7vnet-sidecar1-southeastasia"
   address_prefixes     = ["10.0.4.96/28"]
+  delegation {
+    name = "dns-outbound-delegation"
+    service_delegation {
+      name    = "Microsoft.Network/dnsResolvers"
+      actions = ["Microsoft.Network/virtualNetworks/subnets/join/action"]
+    }
+  }
 }
