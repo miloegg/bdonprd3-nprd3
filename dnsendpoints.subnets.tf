8fd00cc267e24c8672b869d6d215710b2ddc96c6
chiancw <chiancw@gmail.com>
2025-06-04 14:18:17 +0800

    use variables for rg and vnet name in dns subnet

--- dnsendpoints.subnets.tf
+++ dnsendpoints.subnets.tf
@@ -1,6 +1,6 @@
 resource "azurerm_subnet" "nprd_dns_inbound_subnet" {
   name                 = "nprd-dns-inbound_subnet"
-  resource_group_name  = "rgbdo7-hub1-$${starter_location_01}"
-  virtual_network_name = "bdo7vnet-sidecar1-$${starter_location_01}"
+  resource_group_name  = var.custom_replacements.names.connectivity_hub_primary_resource_group_name
+  virtual_network_name = var.custom_replacements.names.primary_sidecar_virtual_network_name
   address_prefixes     = ["10.0.4.80/28"]
 }
