eefa72797da3eb825f47900d8ca4885849925b57
chiancw <chiancw@gmail.com>
2025-06-06 10:13:10 +0800

    add sidecar vnet peering to pri hub

--- sidecarpeering.tf
+++ sidecarpeering.tf
@@ -1,8 +1,8 @@
 # locals to construct the resource ID of side car vnet, not required if side car vnet is exported as output
 locals {
   connectivity_subscription_id = "0fb0d3b2-5b3a-4db0-8e5e-d5459592651d"
-  hub_resource_group_name      = "rgbdo7-hub1-southeastasia"
-  sidecar_vnet_name            = "bdo7vnet-sidecar1-southeastasia"
+  pri_hub_resource_group_name      = "rgbdo7-hub1-southeastasia"
+  pri_sidecar_vnet_name            = "bdo7vnet-sidecar1-southeastasia"
 }
 
 
@@ -11,7 +11,7 @@ resource "azurerm_virtual_hub_connection" "primary_sidecar_to_primary_vhub" {
   provider                  = azurerm.connectivity
   name                      = "pri-sidecar-vhub-connection"
   virtual_hub_id            = virtual_wan_virtual_hub_resource_ids["primary"]
-  remote_virtual_network_id = "/subscriptions/${local.connectivity_subscription_id}/resourceGroups/${local.hub_resource_group_name}/providers/Microsoft.Network/virtualNetworks/${local.sidecar_vnet_name}"
+  remote_virtual_network_id = "/subscriptions/${local.connectivity_subscription_id}/resourceGroups/${local.pri_hub_resource_group_name}/providers/Microsoft.Network/virtualNetworks/${local.pri_sidecar_vnet_name}"
 }
 
 
