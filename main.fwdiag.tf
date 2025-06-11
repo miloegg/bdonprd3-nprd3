fa9823ae482c2102887d1ec7860264312844a330
chiancw <chiancw@gmail.com>
2025-06-11 09:27:51 +0800

    update law resource id for case sensitive bug

--- main.fwdiag.tf
+++ main.fwdiag.tf
@@ -19,7 +19,9 @@ locals {
       metric_categories              = ["AllMetrics"]
       log_analytics_destination_type = "Dedicated"
       # workspace_resource_id          = "/subscriptions/1543411c-a55d-46d6-93bf-2e585fd51376/resourceGroups/rgbdo7-management-southeastasia/providers/Microsoft.OperationalInsights/workspaces/bdo7law-management-southeastasia"
-      workspace_resource_id = module.config.management_group_settings.policy_default_values.log_analytics_workspace_id
+      # workspace_resource_id = module.config.management_group_settings.policy_default_values.log_analytics_workspace_id
+      # adjusted output due to API case sensitive bug expecting resourceGroups instead of resourcegroups
+      workspace_resource_id = replace(module.config.management_group_settings.policy_default_values.log_analytics_workspace_id, "resourcegroups", "resourceGroups")
     }
   }
 }  
